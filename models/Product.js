1134fe5279606f4045155036fd6c018e266e1d24
keerthangowda694-max <keerthangowda694@gmail.com>
2025-11-29 21:15:17 +0530

    Refactor Product schema with additional fields
    Updated product schema to include new fields and timestamps.

--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-    title: String,
-    price: Number,
+const ProductSchema = new mongoose.Schema({
+    name: String,
     description: String,
-    image: String
-});
+    price: Number,
+    imageUrl: String,
+    sold: {
+        type: Boolean,
+        default: false
+    },
+    buyer: {
+        type: String,
+        default: null
+    }
+}, { timestamps: true });
 
-module.exports = mongoose.model("Product", productSchema);
+module.exports = mongoose.model("Product", ProductSchema);
